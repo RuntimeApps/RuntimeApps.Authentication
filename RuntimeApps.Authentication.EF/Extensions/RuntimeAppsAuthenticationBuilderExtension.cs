cc49daa20c8fa8d9afbd0d2adcb19ff7a5a15912
Amin <Amin@AMIN-PC>
2022-10-27 16:47:27 +0200

    Change to .Net UserStore

--- RuntimeApps.Authentication.EF/Extensions/RuntimeAppsAuthenticationBuilderExtension.cs
+++ RuntimeApps.Authentication.EF/Extensions/RuntimeAppsAuthenticationBuilderExtension.cs
@@ -9,8 +9,22 @@ namespace RuntimeApps.Authentication.EF.Extensions {
             where TUser : IdentityUser<TKey>, new()
             where TRole : IdentityRole<TKey>
             where TKey : IEquatable<TKey> {
-            builder.Services.AddScoped<IUserStore<TUser>, EfUserStore<TUser, TRole, TKey>>();
-            builder.Services.AddScoped<IdentityDbContext<TUser, TRole, TKey>>(p => p.GetService<TDbContext>());
+            builder.Services.AddScoped<IUserStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserLoginStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserClaimStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserPasswordStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserSecurityStampStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserEmailStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserLockoutStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserPhoneNumberStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IQueryableUserStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IQueryableUserStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserTwoFactorStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserAuthenticationTokenStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserAuthenticatorKeyStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserTwoFactorRecoveryCodeStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IProtectedUserStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
+            builder.Services.AddScoped<IUserRoleStore<TUser>, UserStore<TUser, TRole, TDbContext, TKey>>();
             return builder;
         }
     }
