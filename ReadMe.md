dc4d8a5450187f529570a3e539402cdfbbe283c0
Amin <Amin@AMIN-PC>
2022-10-27 16:11:18 +0200

    Test added

--- ReadMe.md
+++ ReadMe.md
@@ -1,17 +1,17 @@
 # RuntimeApps.Authentication
 
-The UserManager of ASP.Net core is good for most of porposes and it implements in various type of [Databases](https://github.com/dotnet/aspnetcore/tree/main/src/Identity#community-maintained-store-providers).
+The UserManager of ASP.Net core is good for most of the porposes and it implements in various types of [Databases](https://github.com/dotnet/aspnetcore/tree/main/src/Identity#community-maintained-store-providers).
 
-But there is some problems in this implementation. I list some of them that I have tried to improve it:
+But there are some problems with this implementation. I list some of them that I have tried to improve it:
 
-- The Identity server of ASP.Net core has used [Identity Server 4](https://github.com/IdentityServer/IdentityServer4) however it has been archived and moved to [Duende Software](https://github.com/duendesoftware) which is not free. But I beleive in most cases softwares doesn't need Identity Server and They want to have simple user managment. So this repository only sopport simple usage of user managers based on  that might be useful for your project.
-- The default implementation of ASP.Net core user manager has been depended to MVC (implements razor pages). This repository focuses on Web API applications that has seprate frontend and backend code. So you can esaly use APIs on user managers and external logins.
-- Some implemetations of ASP.Net core user management should be improvied. For example, `UserManager<TUser>` doesn't inharent any interfaces which makes very hard to write tests. In this repository I have tried to solve some of these problems.
-- Asp.Net core external social logins works based on redirect to social login page which is not useful in ASP.Net core Web APIs. ASP.Net core implements this by using MVC and managing the request responses, But this repository has implementes social login token validation which is useful for APIs that doesn't access user pages (Mobile apps and frontend teches).
+- The Identity server of ASP.Net core has used [Identity Server 4](https://github.com/IdentityServer/IdentityServer4) however it has been archived and moved to [Duende Software](https://github.com/duendesoftware) which is not free. But I believe in most cases software doesn't need an Identity Server and They want to have simple user management. So this repository only supports simple usage of user managers based on what might be useful for your project.
+- The default implementation of ASP.Net core user manager has been dependent on MVC (implements razor pages). This repository focuses on Web API applications that have separate frontend and backend code. So you can easily use APIs on user managers and external logins.
+- Some implementations of ASP.Net core user management should be improved. For example, `UserManager<TUser>` doesn't inherit any interfaces which makes it very hard to write tests. In this repository, I have tried to solve some of these problems.
+- Asp.Net core external social logins works based on redirecting to the social login page which is not useful in ASP.Net core Web APIs. ASP.Net core implements this by using MVC and managing the request responses, But this repository has implemented social login token validation which is useful for APIs that don't access user pages (Mobile apps and frontend frameworks).
 
 ## Getting Started
 
-For using this library do the steps:
+To use this library do the steps:
 
 ### Install packages:
 
@@ -19,17 +19,17 @@ Install `RuntimeApps.Authentication` package which implements base services.
 
 ```Install-Package RuntimeApps.Authentication```
 
-If you want to use EntityFrameworkCore implemetation of Authentication install the pcakge
+If you want to use EntityFrameworkCore implementation of Authentication install the package
 
 ```Install-Package RuntimeApps.Authentication.EF```
 
-Or you can install other implementation of store [link](https://github.com/dotnet/aspnetcore/tree/main/src/Identity#community-maintained-store-providers)
+Or you can install other implementations of the store [link](https://github.com/dotnet/aspnetcore/tree/main/src/Identity#community-maintained-store-providers)
 
 ### Create needed classes
 
-1. Create `AccountController` class which inharient `BaseAccountController` and define route and other implemetation if you need.
+1. Create `AccountController` class which inherit `BaseAccountController` and define the route and other implementation if you need them.
 
-2. Create your own `ApplicationDbContext` and inharient `IdentityDbContext`
+2. Create your own `ApplicationDbContext` and inherent `IdentityDbContext`
 
 ### Configure Services
 
@@ -58,18 +58,18 @@ builder.Services.AddAuthentication()
     });
 ```
 
-You can add more services and customise service. Also there is some [samples](./Samples/) in this repository.
+You can add more services and customize services. Also, there are some [samples](./Samples/) in this repository.
 
 ## External Social Logins
 
-In many cases you want to add social login to your project, so users can easily login and use the application. External login needs to redirect user to the social login pages which is not useful for API services. This repository provides easy way to handling external login in ASP.Net core web API. You only shoud do:
+In many cases you want to add social login to your project, so users can quickly log in and use the application. External login needs to redirect the user to the social login pages which are not useful for API services. This repository provides an easy way to handle external login in ASP.Net core web API. You only should do:
 
-In frontend part you could use various library based on what framework you are using. For example you could use [Angular Social Login](https://www.npmjs.com/package/@abacritt/angularx-social-login) in angular and [react-social-login
-](https://www.npmjs.com/package/react-social-login) in react. These packages get a token to client, The only thing that frontend should do is that send the token to API and get application usage token. The implemeted API validate and generate needed data.
+In the frontend part, you could use various libraries based on what framework you are using. For example, you could use [Angular Social Login](https://www.npmjs.com/package/@abacritt/angularx-social-login) in angular and [react-social-login
+](https://www.npmjs.com/package/react-social-login) in react. These packages get a token from the client, The only thing that frontend should do is send the token to API and get the application usage token. The implemented API validates and generates needed data.
 
-In backend we provide some external login validator. each validator has a configuration that needs to be added to DI.
+In the backend, we provide some external login validators. each validator has a configuration that needs to be added to DI.
 
-- **Google Authentication:** For adding google authentication to your api you need to create the Google OAuth 2.0 Client ID and secret [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/google-logins?view=aspnetcore-6.0#create-the-google-oauth-20-client-id-and-secret)] and store them in configuration [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/google-logins?view=aspnetcore-6.0#store-the-google-client-id-and-secret)]. Finaly you should add the configuration and service to DI by using these code:
+- **Google Authentication:** For adding google authentication to your API you need to create the Google OAuth 2.0 Client ID and secret [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/google-logins?view=aspnetcore-6.0#create-the-google-oauth-20-client-id-and-secret)] and store them in configuration [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/google-logins?view=aspnetcore-6.0#store-the-google-client-id-and-secret)]. Finally you should add the configuration and service to DI by using these code:
 
 ```cs
 .AddGoogleExternalLogin(option => {
@@ -79,7 +79,7 @@ In backend we provide some external login validator. each validator has a config
     })
 ```
 
-- **Facebook Authentication:** For adding facebook authentication to your api you need to create the app in facebook [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/facebook-logins?view=aspnetcore-6.0#create-the-app-in-facebook)] and store them in configuration [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/facebook-logins?view=aspnetcore-6.0#store-the-facebook-app-id-and-secret)]. Finaly you should add the configuration and service to DI by using these code:
+- **Facebook Authentication:** For adding Facebook authentication to your API you need to create the app in Facebook [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/facebook-logins?view=aspnetcore-6.0#create-the-app-in-facebook)] and store them in configuration [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/facebook-logins?view=aspnetcore-6.0#store-the-facebook-app-id-and-secret)]. Finally you should add the configuration and service to DI by using these code:
 
 ```cs
 .AddFacebookExternalLogin(option => {
@@ -89,7 +89,7 @@ In backend we provide some external login validator. each validator has a config
     })
 ```
 
-- **Microsoft Authentication:** For adding microsoft authentication to your api you need to create the app in microsoft developer portal [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/microsoft-logins?view=aspnetcore-6.0#create-the-app-in-microsoft-developer-portal)] and store them in configuration [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/microsoft-logins?view=aspnetcore-6.0#store-the-microsoft-client-id-and-secret)]. Finaly you should add the configuration and service to DI by using these code:
+- **Microsoft Authentication:** For adding Microsoft authentication to your API you need to create the app in Microsoft developer portal [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/microsoft-logins?view=aspnetcore-6.0#create-the-app-in-microsoft-developer-portal)] and store them in configuration [[link](https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/microsoft-logins?view=aspnetcore-6.0#store-the-microsoft-client-id-and-secret)]. Finally you should add the configuration and service to DI by using these code:
 
 ```cs
 .AddMicrosoftExternalLogin(option => {
@@ -99,7 +99,7 @@ In backend we provide some external login validator. each validator has a config
     })
 ```
 
-- **Custome Othentication**: If you want to add other external login to your application you should implement [`IExternalLoginProvider`](RuntimeApps.Authentication/Interface/IExternalLoginProvider.cs) for your external login. Then add it to DI
+- **Custome Authentication**: If you want to add other external login to your application you should implement [`IExternalLoginProvider`](RuntimeApps.Authentication/Interface/IExternalLoginProvider.cs) for your external login. Then add it to DI
 
 ```cs
 builder.Services.AddTransient<IExternalLoginProvider, CustomeExternalLoginProvider>();
---
+++ Tests/RuntimeApps.Authentication.Test/UserAccountServiceTest.cs
@@ -0,0 +1,312 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using RuntimeApps.Authentication.Interface;
+using RuntimeApps.Authentication.Service;
+
+namespace RuntimeApps.Authentication.Test {
+    public class UserAccountServiceTest {
+        private const IdentityUser nullUSer = null;
+        private readonly Mock<IUserManager<IdentityUser>> _userManagerMock;
+        private readonly Mock<IJwtProvider<IdentityUser>> _jwtProviderMock;
+        private readonly Mock<IExternalLoginProvider<IdentityUser>> _externalLoginProvider1;
+        private readonly Mock<IExternalLoginProvider<IdentityUser>> _externalLoginProvider2;
+        private readonly IUserAccountService<IdentityUser> _userAccountService;
+
+        public UserAccountServiceTest() {
+            _userManagerMock = new Mock<IUserManager<IdentityUser>>();
+            _jwtProviderMock = new Mock<IJwtProvider<IdentityUser>>();
+            _externalLoginProvider1 = new Mock<IExternalLoginProvider<IdentityUser>>();
+            _externalLoginProvider2 = new Mock<IExternalLoginProvider<IdentityUser>>();
+            _userAccountService = new UserAccountService<IdentityUser, string>(_userManagerMock.Object, _jwtProviderMock.Object, new IExternalLoginProvider<IdentityUser>[] {
+                _externalLoginProvider1.Object,
+                _externalLoginProvider2.Object
+            });
+        }
+
+        [Fact]
+        public async Task Login_Success() {
+            //Arrange
+            string userName = "userName", password = "password";
+            var user = new IdentityUser() {
+                Id = "userId",
+                UserName = userName,
+            };
+            var token = new Model.Token() {
+                AuthenticationToken = "",
+                ExpireDate = DateTimeOffset.Now.AddDays(30),
+            };
+            _userManagerMock.Setup(u => u.FindByNameAsync(userName))
+                .ReturnsAsync(user);
+            _userManagerMock.Setup(u => u.CheckPasswordAsync(user, password))
+                .ReturnsAsync(true);
+            _jwtProviderMock.Setup(t => t.GenerateToken(user))
+                .Returns(token);
+
+            //Act
+            var result = await _userAccountService.LoginAsync(userName, password);
+
+            //Assert
+            result.Success.Should().BeTrue();
+            result.Data.Should().NotBeNull()
+                .And.Be(user);
+            result.Meta.Should().NotBeNull()
+                .And.Be(token);
+        }
+
+        [Fact]
+        public async Task Login_WrongUserName() {
+            //Arrange
+            string userName = "userName", password = "password";
+            _userManagerMock.Setup(u => u.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync(nullUSer);
+
+            //Act
+            var result = await _userAccountService.LoginAsync(userName, password);
+
+            //Assert
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeNull();
+            result.Meta.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Login_WrongPassword() {
+            //Arrange
+            string userName = "userName", password = "password";
+            var user = new IdentityUser() {
+                Id = "userId",
+                UserName = userName,
+            };
+            _userManagerMock.Setup(u => u.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync(nullUSer);
+            _userManagerMock.Setup(u => u.CheckPasswordAsync(user, password))
+                .ReturnsAsync(false);
+
+            //Act
+            var result = await _userAccountService.LoginAsync(userName, password);
+
+            //Assert
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeNull();
+            result.Meta.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Register_Success() {
+            //Arrange
+            string password = "password";
+            var user = new IdentityUser() {
+                Email = "test@test.com"
+            };
+            var token = new Model.Token() {
+                AuthenticationToken = "",
+                ExpireDate = DateTimeOffset.Now.AddDays(30),
+            };
+            _userManagerMock.Setup(u => u.FindByNameAsync(user.Email))
+                .ReturnsAsync(nullUSer);
+            _userManagerMock.Setup(u => u.CreateAsync(user, password))
+                .ReturnsAsync(IdentityResult.Success);
+            _jwtProviderMock.Setup(t => t.GenerateToken(user))
+                .Returns(token);
+
+            //Act
+            var result = await _userAccountService.RegisterAsync(user, password);
+
+            //Assert
+            result.Success.Should().BeTrue();
+            result.Data.Should().NotBeNull()
+                .And.Be(user);
+            result.Data.UserName.Should().Be(result.Data.Email);
+            result.Meta.Should().NotBeNull()
+                .And.Be(token);
+        }
+
+        [Fact]
+        public async Task Register_UserExist() {
+            //Arrange
+            string password = "password";
+            var user = new IdentityUser() {
+                Email = "test@test.com"
+            };
+            _userManagerMock.Setup(u => u.FindByNameAsync(user.Email))
+                .ReturnsAsync((IdentityUser)user);
+
+            //Act
+            var result = await _userAccountService.RegisterAsync(user, password);
+
+            //Assert
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeNull();
+            result.Meta.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Register_ValidateError() {
+            //Arrange
+            string password = "password";
+            var user = new IdentityUser() {
+                Email = "test@test.com"
+            };
+            _userManagerMock.Setup(u => u.FindByNameAsync(user.Email))
+                .ReturnsAsync(nullUSer);
+            _userManagerMock.Setup(u => u.CreateAsync(user, password))
+                .ReturnsAsync(IdentityResult.Failed(new IdentityError[] {
+                    new IdentityError {
+                        Code = "InvalidPassword",
+                        Description = "Password must be more than 6 character"
+                    }
+                }));
+
+            //Act
+            var result = await _userAccountService.RegisterAsync(user, password);
+
+            //Assert
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeNull();
+            result.Meta.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task ExternalLogin_Success_CreateNewUser() {
+            //Arrange
+            string provider = "Google", token = "token";
+            var user = new IdentityUser() {
+                Email = "test@test.com"
+            };
+            var appToken = new Model.Token() {
+                AuthenticationToken = "",
+                ExpireDate = DateTimeOffset.Now.AddDays(30),
+            };
+            var loginInfo = new UserLoginInfo(provider, token, provider);
+            _externalLoginProvider1.SetupGet(e => e.Provider)
+                .Returns("Not Selected Provided");
+            _externalLoginProvider2.SetupGet(e => e.Provider)
+                .Returns(provider);
+            _externalLoginProvider2.Setup(e => e.ValidateAsync(token))
+                .ReturnsAsync((user, loginInfo));
+            _userManagerMock.Setup(u => u.FindByLoginAsync(provider, token))
+                .ReturnsAsync(nullUSer);
+            _userManagerMock.Setup(u => u.FindByEmailAsync(user.Email))
+                .ReturnsAsync(nullUSer);
+            _userManagerMock.Setup(u => u.CreateAsync(user))
+                .ReturnsAsync(IdentityResult.Success);
+            _userManagerMock.Setup(u => u.AddLoginAsync(user, loginInfo))
+                .ReturnsAsync(IdentityResult.Success);
+            _jwtProviderMock.Setup(t => t.GenerateToken(user))
+                .Returns(appToken);
+
+            //Act
+            var result = await _userAccountService.ExternalLoginAsync(provider, token);
+
+            //Assert
+            result.Success.Should().BeTrue();
+            result.Data.Should().NotBeNull()
+                .And.Be(user);
+            result.Meta.Should().NotBeNull()
+                .And.Be(appToken);
+            _userManagerMock.Verify(u => u.CreateAsync(It.IsAny<IdentityUser>()), Times.Once());
+            _userManagerMock.Verify(u => u.AddLoginAsync(It.IsAny<IdentityUser>(), It.IsAny<UserLoginInfo>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task ExternalLogin_Success_ExistUser() {
+            //Arrange
+            string provider = "Google", token = "token";
+            var user = new IdentityUser() {
+                Email = "test@test.com"
+            };
+            var appToken = new Model.Token() {
+                AuthenticationToken = "",
+                ExpireDate = DateTimeOffset.Now.AddDays(30),
+            };
+            var loginInfo = new UserLoginInfo(provider, token, provider);
+            _externalLoginProvider1.SetupGet(e => e.Provider)
+                .Returns("Not Selected Provided");
+            _externalLoginProvider2.SetupGet(e => e.Provider)
+                .Returns(provider);
+            _externalLoginProvider2.Setup(e => e.ValidateAsync(token))
+                .ReturnsAsync((user, loginInfo));
+            _userManagerMock.Setup(u => u.FindByLoginAsync(provider, token))
+                .ReturnsAsync(nullUSer);
+            _userManagerMock.Setup(u => u.FindByEmailAsync(user.Email))
+                .ReturnsAsync(user);
+            _userManagerMock.Setup(u => u.AddLoginAsync(user, loginInfo))
+                .ReturnsAsync(IdentityResult.Success);
+            _jwtProviderMock.Setup(t => t.GenerateToken(user))
+                .Returns(appToken);
+
+            //Act
+            var result = await _userAccountService.ExternalLoginAsync(provider, token);
+
+            //Assert
+            result.Success.Should().BeTrue();
+            result.Data.Should().NotBeNull()
+                .And.Be(user);
+            result.Meta.Should().NotBeNull()
+                .And.Be(appToken);
+            _userManagerMock.Verify(u => u.CreateAsync(It.IsAny<IdentityUser>()), Times.Never());
+            _userManagerMock.Verify(u => u.AddLoginAsync(It.IsAny<IdentityUser>(), It.IsAny<UserLoginInfo>()), Times.Once());
+        }
+
+        [Fact]
+        public async Task ExternalLogin_Success_ExistsLogin() {
+            //Arrange
+            string provider = "Google", token = "token";
+            var user = new IdentityUser() {
+                Email = "test@test.com"
+            };
+            var appToken = new Model.Token() {
+                AuthenticationToken = "",
+                ExpireDate = DateTimeOffset.Now.AddDays(30),
+            };
+            var loginInfo = new UserLoginInfo(provider, token, provider);
+            _externalLoginProvider1.SetupGet(e => e.Provider)
+                .Returns("Not Selected Provided");
+            _externalLoginProvider2.SetupGet(e => e.Provider)
+                .Returns(provider);
+            _externalLoginProvider2.Setup(e => e.ValidateAsync(token))
+                .ReturnsAsync((user, loginInfo));
+            _userManagerMock.Setup(u => u.FindByLoginAsync(provider, token))
+                .ReturnsAsync(user);
+            _userManagerMock.Setup(u => u.AddLoginAsync(user, loginInfo))
+                .ReturnsAsync(IdentityResult.Success);
+            _jwtProviderMock.Setup(t => t.GenerateToken(user))
+                .Returns(appToken);
+
+            //Act
+            var result = await _userAccountService.ExternalLoginAsync(provider, token);
+
+            //Assert
+            result.Success.Should().BeTrue();
+            result.Data.Should().NotBeNull()
+                .And.Be(user);
+            result.Meta.Should().NotBeNull()
+                .And.Be(appToken);
+            _userManagerMock.Verify(u => u.CreateAsync(It.IsAny<IdentityUser>()), Times.Never());
+            _userManagerMock.Verify(u => u.AddLoginAsync(It.IsAny<IdentityUser>(), It.IsAny<UserLoginInfo>()), Times.Never());
+        }
+
+
+        [Fact]
+        public async Task ExternalLogin_ProviderNotFound() {
+            //Arrange
+            string provider = "NotExistProvider", token = "token";
+            var loginInfo = new UserLoginInfo(provider, token, provider);
+            _externalLoginProvider1.SetupGet(e => e.Provider)
+                .Returns("Provider1");
+            _externalLoginProvider2.SetupGet(e => e.Provider)
+                .Returns("Provider2");
+
+            //Act
+            var result = await _userAccountService.ExternalLoginAsync(provider, token);
+
+            //Assert
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeNull();
+            result.Meta.Should().BeNull();
+            _userManagerMock.Verify(u => u.CreateAsync(It.IsAny<IdentityUser>()), Times.Never());
+            _userManagerMock.Verify(u => u.AddLoginAsync(It.IsAny<IdentityUser>(), It.IsAny<UserLoginInfo>()), Times.Never());
+        }
+    }
+}
